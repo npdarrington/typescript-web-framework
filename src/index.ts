80254cdbbfa9f97dd950b8d06e9c53277f40be5b
Nathan Darrington <npdarrington@icloud.com>
2021-10-22 17:23:21 -0600

    Remove event testing code

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 0 });
-
-user.events.on('change', () => {
-	console.log('Change has registered!');
-});
-
-user.events.on('change', () => {
-	console.log('This change is not a duplication');
-});
-
-user.events.trigger('change');
