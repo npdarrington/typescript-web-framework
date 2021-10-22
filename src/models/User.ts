0dcf3adfc408bfaf0aa9278754d87cd9f0276b0a
Nathan Darrington <npdarrington@icloud.com>
2021-10-22 12:22:12 -0600

    Move user event logic to Eventing class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
 
 interface UserProps {
 	id?: number;
@@ -6,14 +7,8 @@ interface UserProps {
 	age?: number;
 }
 
-type Callback = () => void;
-
-type Events = {
-	[key: string]: Callback[];
-};
-
 export class User {
-	events: Events = {};
+	events: Eventing = new Eventing();
 
 	constructor(private data: UserProps) {}
 
@@ -23,22 +18,6 @@ export class User {
 
 	set(update: UserProps): void {
 		Object.assign(this.data, update);
-	}
-
-	on(eventName: string, callback: Callback): void {
-		const handlers = this.events[eventName] || [];
-		handlers.push(callback);
-		this.events[eventName] = handlers;
-	}
-
-	trigger(eventName: string): void {
-		const handlers = this.events[eventName];
-
-		if (!handlers || handlers.length === 0) {
-			return;
-		}
-
-		handlers.forEach(callback => callback());
 	}
 
 	fetch(): void {
