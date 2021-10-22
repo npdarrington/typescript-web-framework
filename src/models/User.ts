8f3bdc445994beaf3d4d1b07658597b08b2cc18f
Nathan Darrington <npdarrington@icloud.com>
2021-10-22 08:38:06 -0600

    User get and set methods

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,4 +5,12 @@ interface UserProps {
 
 export class User {
 	constructor(private data: UserProps) {}
+
+	get(propName: string): string | number {
+		return this.data[propName];
+	}
+
+	set(update: UserProps): void {
+		Object.assign(this.data, update);
+	}
 }
