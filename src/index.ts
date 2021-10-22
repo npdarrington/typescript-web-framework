dd768b1ae45e651474b7f77341e84f6d0782bd9a
Nathan Darrington <npdarrington@icloud.com>
2021-10-22 08:40:05 -0600

    Entry index.ts file that test .on storing properly

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { User } from './models/User';
+
+const user = new User({ name: 'myname', age: 20 });
+
+user.on('change', () => {});
+user.on('change', () => {});
+user.on('click', () => {});
+
+console.log(user);
