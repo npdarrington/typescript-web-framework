c1c1dcf8a8a27200a22e682ba5ad161883e69c00
Nathan Darrington <npdarrington@icloud.com>
2021-10-23 08:34:39 -0600

    testing user save

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'new record', age: 0 });
+const user = new User({ id: 1, name: 'newer name', age: 9999 });
+
+// user.on('save', () => {
+// 	console.log(user);
+// });
+
+// user.save();
