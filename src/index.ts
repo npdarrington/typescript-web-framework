bf475343fee40dcbb2e766f10320965edfa83b57
Nathan Darrington <npdarrington@icloud.com>
2021-10-24 11:11:33 -0600

    Update test file to test rendering UserEdit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
 
 const user = User.buildUser({ name: 'NAME', age: 30 });
 const root = document.getElementById('root');
 
 if (root) {
-	const userForm = new UserForm(root, user);
-	userForm.render();
+	const userEdit = new UserEdit(root, user);
+	userEdit.render();
 } else {
 	throw new Error('Root element not found.');
 }
