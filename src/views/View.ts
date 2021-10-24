48ef825e67fc3bd4a809c206045899091ba7d614
Nathan Darrington <npdarrington@icloud.com>
2021-10-24 10:48:32 -0600

    mapRegions will get all elements within the fragment and populate regionsMap object

--- src/views/View.ts
+++ src/views/View.ts
@@ -33,11 +33,24 @@ export abstract class View<T extends Model<K>, K> {
 		}
 	}
 
+	mapRegions(fragment: DocumentFragment): void {
+		const regionsMap = this.regionsMap();
+
+		for (let key in regionsMap) {
+			const selector = regionsMap[key];
+			const element = fragment.querySelector(selector);
+			if (element) {
+				this.regions[key] = element;
+			}
+		}
+	}
+
 	render(): void {
 		this.parent.innerHTML = '';
 		const templateElement = document.createElement('template');
 		templateElement.innerHTML = this.template();
 		this.bindEvents(templateElement.content);
+		this.mapRegions(templateElement.content);
 		this.parent.append(templateElement.content);
 	}
 }
