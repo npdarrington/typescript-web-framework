aa172564e1ddddf3984b1629f095060c524d40d4
Nathan Darrington <npdarrington@icloud.com>
2021-10-24 09:00:56 -0600

    Generate tsconfig for strict checks and set type guards are possible null refs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import { UserForm } from './views/UserForm';
 import { User } from './models/User';
 
-const collection = User.buildUserCollection();
+const user = User.buildUser({ name: 'NAME', age: 30 });
+const root = document.getElementById('root');
 
-collection.on('change', () => {
-	console.log(collection);
-});
-
-collection.fetch();
+if (root) {
+	const userForm = new UserForm(root, user);
+	userForm.render();
+} else {
+	throw new Error('Root element not found.');
+}
